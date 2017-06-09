d28792394d7a6d79909481fa439b0604e0497b5f
App Academy Student <student@appacademy.io>
2017-06-08 17:56:03 -0700

    Complete max windowed range

--- max_windowed_range.rb
+++ max_windowed_range.rb
@@ -1,3 +1,4 @@
+require_relative 'stacks_and_queue'
 def naive_max_windowed_range(arr, w)
   idx = 0
   max_range = 0
@@ -10,10 +11,23 @@ def naive_max_windowed_range(arr, w)
 
   max_range
 end
-
+# require 'byebug'
 def optimized_max_window(arr, w)
-  current_window = 
+  # debugger
+  queue = MinMaxStackQueue.new
+  w.times {|i| queue.enqueue(arr[i])}
+  max_range = queue.max - queue.min
 
+  idx = w
+  while idx < arr.length
+    queue.dequeue
+    queue.enqueue(arr[idx])
+    current_range = queue.max - queue.min
+    max_range = current_range if current_range > max_range
+
+    idx += 1
+  end
+  max_range
 end
 
-p naive_max_windowed_range([1, 2, 3, 5], 3)
+p optimized_max_window([1, 2, 3, 5], 3)
